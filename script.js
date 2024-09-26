96b86ba301d18bb064d7dc54a02fcf2bc5abcb0c
Emperor01-tech <oluwasegundekoya10@gmail.com>
2024-09-26 22:57:35 +0100

    Update and rename Script.js to script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Placeholder for future functionality
+document.getElementById("join-quiz").addEventListener("click", function() {
+    alert("Joining a quiz match! (Functionality to be implemented)");
+});
+
+// Additional interactive features can be added for admin login, quiz management, etc.
+
